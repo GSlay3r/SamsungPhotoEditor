b39213ef9986c0f3e2a67881f92c7cc163f24269
GSlay3r <sasha.vasilevvs@yandex.ru>
2023-05-27 12:23:43 +0300

    Second

--- app/java/com/example/photoeditor/core/libs/PhotoEditor/filters/FilterListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.photoeditor.core.libs.PhotoEditor.filters;
-
-import ja.burhanrashid52.photoeditor.PhotoFilter;
-
-public interface FilterListener {
-    void onFilterSelected(PhotoFilter photoFilter);
-}
